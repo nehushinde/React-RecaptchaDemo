085e32cb0b37ec04c7766dcfa41500bca27ce100
Neha <neha.shinde@siriusemb.com>
2018-10-17 15:51:15 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Recaptcha from 'react-recaptcha';
 import './App.css';
 
 class App extends Component {
+  state = {
+    isVerified : false
+  }
+
+  recaptchaLoaded = () => {
+    console.log('captcha loaded')
+  }
+
+  handleSubscribe = ( ) => {
+    if (this.state.isVerified) {
+      alert('You have successfully subscribed!')
+    } else {
+      alert('Please verify that you are human!')
+    }
+  } 
+
+  verifyCallback = (response) => {
+    if(response) {
+      this.setState({
+        isVerified: true
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div>
+            <input type="text" placeholder="mail-id" className="Input"></input>
+            <Recaptcha
+                sitekey="6LdIF3UUAAAAAPBCHZv1_awjJsQosJ0O4_kSkxvK"
+                render="explicit"
+                verifyCallback={this.verifyCallback}
+                onloadCallback={this.recaptchaLoaded}
+              />
+            <div onClick={this.handleSubscribe}>Subscribe</div>
+          </div>
       </div>
     );
   }
